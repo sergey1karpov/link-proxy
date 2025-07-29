1533c4e8c19c08193c1606bb1522decace14ac70
Karpov Sergey <karpovsergey@gmail.com>
2025-07-29 15:55:08 +0300

    Create entities

--- src/main/java/com/linker/linker/entity/Link.java
+++ src/main/java/com/linker/linker/entity/Link.java
@@ -1,4 +1,40 @@
 package com.linker.linker.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "links")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Link {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "old_url")
+    private String oldUrl;
+
+    @Column(name = "new_url")
+    private String newUrl;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Column(name = "time_to_leave")
+    private LocalDateTime timeToLeave;
+
+    @ManyToMany(mappedBy = "links")
+    private List<User> users = new ArrayList<>();
+
+    enum Status {
+        PUBLIC, PRIVATE
+    }
 }
--- src/main/java/com/linker/linker/entity/User.java
+++ src/main/java/com/linker/linker/entity/User.java
@@ -1,4 +1,43 @@
 package com.linker.linker.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_links",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "link_id")
+    )
+    private List<Link> links = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    enum Role {
+        USER_ROLE, ADMIN_ROLE
+    }
 }
